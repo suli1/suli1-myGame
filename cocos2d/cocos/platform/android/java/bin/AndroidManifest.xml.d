26ff5ae6606f1de82af77b02bf5a114ce776a039
suli <sss908938889@163.com>
2014-08-17 00:36:07 +0800

    add Astart and fix bugs

--- cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-E:\home\MyGame\MyGame\cocos2d\cocos\platform\android\java\bin\AndroidManifest.xml : \
-E:\home\MyGame\MyGame\cocos2d\cocos\platform\android\java\AndroidManifest.xml \
--- cocos2d/cocos/platform/android/java/gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : E:\home\MyGame\MyGame\cocos2d\cocos\platform\android\java\bin\AndroidManifest.xml \
